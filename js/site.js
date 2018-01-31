0c50b84c663ace9f151486de817a725262c7ea76
King Arther <kingarther2@hotmail.com>
2018-01-30 22:18:04 -0600

    validate if an email address is entered

--- js/site.js
+++ js/site.js
@@ -5,11 +5,18 @@ jQuery(function($) {
 
   $('#form').on("submit", function(e) {
     var email = $('#email').val();
+    var valid =  /.+@.+/;
     e.preventDefault();
     if (email == '') {
       console.log('email field is empty');
       $('#invalid').remove();
       $('#form-list').append('<li id="invalid">Please enter your email address to recieve your invite.</li>');
     }
+    else if (!valid.test(email)) {
+        console.log('invalid email');
+        $('#invalid').remove();
+        $('#form-list').append('<li id="invalid">Please enter a valid email address.</li>');
+        return false;
+      }
   });
 });
