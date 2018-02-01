d004427a22638168071ddcc1369c5b39276aee12
King Arther <kingarther2@hotmail.com>
2018-02-01 07:48:44 -0600

    fix indentation, eslinted

--- js/site.js
+++ js/site.js
@@ -7,20 +7,20 @@ jQuery(function($) {
     var email = $('#email').val();
     var valid =  /.+@.+/;
     e.preventDefault();
-    if (email == '') {
+    if (email === '') {
       console.log('email field is empty');
       $('#invalid').remove();
       $('#form-list').append('<li id="invalid">Please enter your email address to recieve your invite.</li>');
     }
     else if (!valid.test(email)) {
-        console.log('invalid email');
-        $('#invalid').remove();
-        $('#form-list').append('<li id="invalid">Please enter a valid email address.</li>');
-        return false;
-      }
+      console.log('invalid email');
+      $('#invalid').remove();
+      $('#form-list').append('<li id="invalid">Please enter a valid email address.</li>');
+      return false;
+    }
     else {
-        $(this).remove();
-        $('#description').append('<li id="submitted">We\'ll see how good you are in the Bog this Friday.</li>');
-      }
+      $(this).remove();
+      $('#description').append('<li id="submitted">We\'ll see how good you are in the Bog this Friday.</li>');
+    }
   });
 });
